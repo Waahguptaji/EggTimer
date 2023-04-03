a124a52ff3ad94a9ecee11817d0a8a20ef0b108f
sahilgupta <sahilgupta742@gmail.com>
2023-04-03 23:42:24 +0530

    Egg Timer

---
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.eggtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Boolean counterIsActive = false;
+    Button button;
+    CountDownTimer countDownTimer;
+
+
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        timerSeekBar.setEnabled(true);
+        countDownTimer.cancel();
+        button.setText("GO!");
+        counterIsActive = false;
+    }
+
+
+
+
+    public void buttonClicked(View view){
+
+        if (counterIsActive) {
+            resetTimer();;
+        }else {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);//once the timer statrted we can move the seek bar
+            button.setText("STOP!");
+        }
+
+       countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 150 , 1000){
+
+           @Override
+           public void onTick(long l) {
+               updateTimer((int) l/1000);//it update the timer
+           }
+
+           @Override
+           public void onFinish() {
+               MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
+               mediaPlayer.start();
+               resetTimer();
+           }
+       }.start();
+
+    }
+
+    public void updateTimer(int secondsLeft){
+        int minutes = secondsLeft/60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if(seconds <= 9){// it is for when the second has two single digit left show one zero
+            secondString = "0" + seconds;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerSeekBar = findViewById(R.id.timerSeekBar);
+        timerTextView = findViewById(R.id.timerTextView);
+        button = findViewById(R.id.button);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                updateTimer(i);//when progress bar change it put the value of i in the method which set the value of text view.
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+
+
+
+    }
+}
